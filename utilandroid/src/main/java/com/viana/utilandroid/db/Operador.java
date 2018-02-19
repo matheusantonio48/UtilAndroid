0269ae6a1c50ffcec2ddebb3fbc25100a1dfd143
João Viana <joao.viana@redeflex.com.br>
2018-02-19 13:07:24 -0400

    Inicial Commit

---
+++ utilandroid/src/main/java/com/viana/utilandroid/db/Operador.java
@@ -0,0 +1,21 @@
+package com.viana.utilandroid.db;
+
+/**
+ * Created by joao.viana on 19/02/2018.
+ */
+
+public enum Operador {
+    Igual,
+    Maior,
+    Menor,
+    MaiorIgual,
+    MenorIgual,
+    Diferente,
+    Inicie,
+    Contenha,
+    Termine,
+    Nulo,
+    NaoNulo,
+    Vazio,
+    DiferenteVazio
+}
