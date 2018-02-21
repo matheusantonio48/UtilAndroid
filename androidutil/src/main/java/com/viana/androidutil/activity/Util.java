626ea166dbb70262e1a3b9e51a0e636a95d705a5
João Viana <joao.viana@redeflex.com.br>
2018-02-21 08:39:20 -0400

    Alteração Util

--- androidutil/src/main/java/com/viana/androidutil/activity/Util.java
+++ androidutil/src/main/java/com/viana/androidutil/activity/Util.java
@@ -3,16 +3,73 @@ package com.viana.androidutil.activity;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+import static com.viana.androidutil.io.Util.getFilename;
 
 /**
  * Created by Joao Viana on 17/02/2018.
  */
 
 public class Util {
-    public static void openNewActivity(Context pContext, Class<?> pClass, boolean pFechar) {
+    public static void openNewActivity(Context pContext, Class<?> pClass, Bundle pParametros, boolean pFechar) throws Exception {
         Intent intent = new Intent(pContext, pClass);
+        if (pParametros != null)
+            intent.putExtras(pParametros);
         ((Activity) pContext).startActivity(intent);
         if (pFechar)
             ((Activity) pContext).finish();
+    }
+
+    public static void openNewActivityForResult(Context pContext, Class<?> pActivity, int pRequestCode, Bundle pParametros) throws Exception {
+        Intent intent = new Intent(pContext, pActivity);
+        if (pParametros != null)
+            intent.putExtras(pParametros);
+        ((Activity) pContext).startActivityForResult(intent, pRequestCode);
+    }
+
+    public static void openDialingScreen(Context pContext, String pNumberPhone) throws Exception {
+        Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + pNumberPhone));
+        pContext.startActivity(intent);
+    }
+
+    public static void shareText(Context pContext, String pTexto) throws Exception {
+        Intent shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.putExtra(Intent.EXTRA_STREAM, pTexto);
+        shareIntent.setType("text/plain");
+        pContext.startActivity(Intent.createChooser(shareIntent, "Compartilhar com"));
+    }
+
+    public static void shareScreen(Context pContext) throws Exception {
+        View view = ((Activity) pContext).getWindow().getDecorView();
+
+        //Cria print
+        DisplayMetrics dm = pContext.getResources().getDisplayMetrics();
+        view.measure(View.MeasureSpec.makeMeasureSpec(dm.widthPixels, View.MeasureSpec.EXACTLY), View.MeasureSpec.makeMeasureSpec(dm.heightPixels, View.MeasureSpec.EXACTLY));
+        view.layout(0, 0, view.getMeasuredWidth(), view.getMeasuredHeight());
+        Bitmap returnedBitmap = Bitmap.createBitmap(view.getMeasuredWidth(), view.getMeasuredHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(returnedBitmap);
+        view.draw(canvas);
+        File fotoFinal = new File(getFilename(pContext));
+        FileOutputStream outStream = new FileOutputStream(fotoFinal);
+        returnedBitmap.compress(Bitmap.CompressFormat.JPEG, 100, outStream);
+        outStream.flush();
+        outStream.close();
+
+        //Compartilhar
+        Intent shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(fotoFinal));
+        shareIntent.setType("image/jpeg");
+        pContext.startActivity(Intent.createChooser(shareIntent, "Compartilhar com"));
     }
 }
--- androidutil/src/main/java/com/viana/androidutil/io/Util.java
+++ androidutil/src/main/java/com/viana/androidutil/io/Util.java
@@ -1,5 +1,9 @@
 package com.viana.androidutil.io;
 
+import android.content.Context;
+import android.os.Environment;
+
+import java.io.File;
 import java.text.ParsePosition;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -49,5 +53,22 @@ public class Util {
             ex.printStackTrace();
             return "";
         }
+    }
+
+    public static String getFilePath(Context pContext) throws Exception {
+        File mediaStorageDir = new File(Environment.getExternalStorageDirectory()
+                + "/Android/data/"
+                + pContext.getPackageName()
+                + "/Files/Compressed");
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists())
+            mediaStorageDir.mkdirs();
+
+        return (mediaStorageDir.getAbsolutePath() + "/");
+    }
+
+    public static String getFilename(Context pContext) throws Exception {
+        String mImageName = "IMG_" + String.valueOf(System.currentTimeMillis()) + ".jpg";
+        return getFilePath(pContext) + mImageName;
     }
 }
